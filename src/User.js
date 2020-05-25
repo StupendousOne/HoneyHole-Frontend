9b299d0898fd4fb6feb73d5f72ba029af959db7e
rpdecks <rpdecks@gmail.com>
2020-05-25 16:27:04 -0400

    users index working with reviews/favorites on expand

--- src/User.js
+++ src/User.js
@@ -6,11 +6,11 @@ class User {
         this.bio = bio
         this.username = username
         this.email = email
-        this.reviews = reviews
-        this.favorite_fishing_spots = favorite_fishing_spots 
         this.element = document.createElement('card')
     }
 
+    // have to pass in spots and reviews as they don't seem to want to come in with the user. =
+    // is this execution context issue? can we bind?
     renderUser(spots, reviews){
         // create card and put user name/username on it
         this.element.className = 'card'
@@ -19,31 +19,55 @@ class User {
         const username = document.createElement('p')
         username.innerText = this.username
     
-        let review_count = document.createElement('li')
+        // create li tags for reviews & spots
+        const review_count = document.createElement('li')
         review_count.innerText = `${reviews.length} reviews`
 
-        let fav_count = document.createElement('li')
+        const fav_count = document.createElement('li')
         fav_count.innerText = `${spots.length} favorited spots`
 
-        // detail user review/favorites
-        // if (Array.isArray(this.reviews) && this.reviews.length) {
-        //     let review_count = document.createElement('li')
-        //     review_count.innerText = `${this.reviews.size} reviews`
-        // } else { 
-        //     let review_count = document.createElement('li')
-        //     review_count.innerText = `${this.name} has no reviews`
-        // }
-        // if (Array.isArray(this.reviews) && this.reviews.length) {
-        //     let fav_count = document.createElement('li')
-        //     fav_count.innerText = `${this.favorite_fishing_spots.size} favorited spots`
-        // } else { 
-        //     let fav_count = document.createElement('li')
-        //     fav_count.innerText = `${this.name} has no favorited spots`
-        // }
+        // expand button for user detail
+        const div = document.createElement('div')
+        const expandBtn = document.createElement('button')
+        expandBtn.innerText = 'Expand'
+        expandBtn.addEventListener('click', () => {
+            this.expandUser(spots, reviews, div)
+        })
 
         // append elemnts to card and return the finished element
-        this.element.append(name, username, review_count, fav_count)
+        this.element.append(name, username, review_count, fav_count, expandBtn, div)
         
         return this.element
+    }
+
+    expandUser(spots, reviews, div) {
+        // wipes children when user clicks expand again (to close)
+        if(div.lastChild) {
+            this.removeChildren(div)
+        // create ul for reviews & spots and appends
+        } else {
+            let rev_ul = document.createElement('ul')
+            reviews.forEach((review) => {
+                let rev_li = document.createElement('li') 
+                rev_li.innerText = review.title
+                rev_ul.appendChild(rev_li)
+            })
+            let spot_ul = document.createElement('ul')
+            spots.forEach((spot) => {
+                let spot_li = document.createElement('li') 
+                spot_li.innerText = spot.name
+                spot_ul.appendChild(spot_li)
+            })
+            div.append(rev_ul, spot_ul)
+            // returns div element to expandUser for appending
+            return div
+        }
+    }
+
+    removeChildren(tag) {
+        while (tag.lastChild) {
+            tag.removeChild(tag.lastChild)
+        }
+        return tag
     }
 }
