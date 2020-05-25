527be81e4c52cabca1693029590697045b900c18
rpdecks <rpdecks@gmail.com>
2020-05-25 12:41:13 -0400

    users displaying as index of cards

--- src/User.js
+++ src/User.js
@@ -1,10 +1,49 @@
 class User {
     
-    constructor(name, bio, username, email) {
+    constructor(name, bio, username, email, reviews, favorite_fishing_spots) {
 
         this.name = name
         this.bio = bio
         this.username = username
         this.email = email
+        this.reviews = reviews
+        this.favorite_fishing_spots = favorite_fishing_spots 
+        this.element = document.createElement('card')
+    }
+
+    renderUser(spots, reviews){
+        // create card and put user name/username on it
+        this.element.className = 'card'
+        const name = document.createElement('p')
+        name.innerText = this.name
+        const username = document.createElement('p')
+        username.innerText = this.username
+    
+        let review_count = document.createElement('li')
+        review_count.innerText = `${reviews.length} reviews`
+
+        let fav_count = document.createElement('li')
+        fav_count.innerText = `${spots.length} favorited spots`
+
+        // detail user review/favorites
+        // if (Array.isArray(this.reviews) && this.reviews.length) {
+        //     let review_count = document.createElement('li')
+        //     review_count.innerText = `${this.reviews.size} reviews`
+        // } else { 
+        //     let review_count = document.createElement('li')
+        //     review_count.innerText = `${this.name} has no reviews`
+        // }
+        // if (Array.isArray(this.reviews) && this.reviews.length) {
+        //     let fav_count = document.createElement('li')
+        //     fav_count.innerText = `${this.favorite_fishing_spots.size} favorited spots`
+        // } else { 
+        //     let fav_count = document.createElement('li')
+        //     fav_count.innerText = `${this.name} has no favorited spots`
+        // }
+
+        // append elemnts to card and return the finished element
+        this.element.append(name, username, review_count, fav_count)
+        
+        return this.element
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
+const BASE_URL = "http:localhost:3000/api/v1/"
 const USER_URL = "http:localhost:3000/api/v1/users/"
 
-function userFetchTest(){
+fetchUsers()
+
+function fetchUsers(){
     fetch(USER_URL)
         .then(res => res.json())
         .then(users => renderUsers(users))
 }
 
 function renderUsers(users){
-    users.forEach(user => renderUser(user))
+    users.forEach((user) => {
+        const userObj = new User(user.name, user.username, user.bio, user.favorite_fishing_spots, user.reviews)
+        const card = userObj.renderUser(user.favorite_fishing_spots, user.reviews)
+        rendersCard(card)
+    })
 }
 
-function renderUser(user){
-    let body = document.querySelector("body")
-    let userLi = document.createElement("li")
-
-    userLi.innerText = user.username
-
-    body.appendChild(userLi)
+function rendersCard(card){
+    const mainContainer = document.querySelector('main')
+    mainContainer.appendChild(card)
 }
-
-userFetchTest()
