cd8995f129a63472eba4c0e7a2cbcd8fa159f0c6
rpdecks <rpdecks@gmail.com>
2020-05-25 10:31:01 -0400

    created User.js and added Poke styling for testing

---
+++ src/User.js
@@ -0,0 +1,10 @@
+class User {
+    
+    constructor(name, bio, username, email) {
+
+        this.name = name
+        this.bio = bio
+        this.username = username
+        this.email = email
+    }
+}
