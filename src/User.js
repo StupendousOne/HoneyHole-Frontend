77c1b1ebabbe2923de6b69189e020283388b84a6
rpdecks <rpdecks@gmail.com>
2020-05-26 09:25:54 -0400

    more semantic stuff focused on user class

--- src/User.js
+++ src/User.js
@@ -6,36 +6,90 @@ class User {
         this.bio = bio
         this.username = username
         this.email = email
-        this.element = document.createElement('card')
+        this.element = document.createElement('div')
     }
+
 
     // have to pass in spots and reviews as they don't seem to want to come in with the user. =
     // is this execution context issue? can we bind?
+    // renderUser(spots, reviews){
+    //     // create card and put user name/username on it
+    //     this.element.className = 'card'
+    //     const name = document.createElement('p')
+    //     name.innerText = this.name
+    //     const username = document.createElement('p')
+    //     username.innerText = this.username
+    
+    //     // create li tags for reviews & spots
+    //     const review_count = document.createElement('li')
+    //     review_count.innerText = `${reviews.length} reviews`
+
+    //     const fav_count = document.createElement('li')
+    //     fav_count.innerText = `${spots.length} favorited spots`
+
+    //     // expand button for user detail
+    //     const div = document.createElement('div')
+    //     const expandBtn = document.createElement('button')
+    //     expandBtn.innerText = 'Expand'
+    //     expandBtn.addEventListener('click', () => {
+    //         this.expandUser(spots, reviews, div)
+    //     })
+
+    //     // append elemnts to card and return the finished element
+    //     this.element.append(name, username, review_count, fav_count, expandBtn, div)
+        
+    //     return this.element
+    // }
+
+    // attempt to add styling below. Copied renderUser to test
+        // <div class="ui fluid container">
+        //     <div class="ui cards">
+        //       <div class="card">
+        //         <div class="content">
+        //           <div class="header">Insert name</div>
+        //           <div class="description">bio goes here</div>
+        //         </div>
+        //         <div class="ui bottom attached button">
+        //           <i class="add icon"></i>
+        //           Button text
+        //         </div>
+        //       </div>
+        //   </div>
+
     renderUser(spots, reviews){
         // create card and put user name/username on it
         this.element.className = 'card'
-        const name = document.createElement('p')
+        const content = document.createElement('div')
+        content.className = 'content'
+        const name = document.createElement('div')
+        name.className = 'header'
         name.innerText = this.name
-        const username = document.createElement('p')
-        username.innerText = this.username
-    
-        // create li tags for reviews & spots
-        const review_count = document.createElement('li')
-        review_count.innerText = `${reviews.length} reviews`
+        const details = document.createElement('div')
+        details.className = 'description'
+        details.innerText = `${this.username} has ${reviews.length} reviews.`
 
-        const fav_count = document.createElement('li')
-        fav_count.innerText = `${spots.length} favorited spots`
+        // // create li tags for reviews & spots
+        // const review_count = document.createElement('li')
+        // review_count.innerText = `${reviews.length} reviews`
+
+        // const fav_count = document.createElement('li')
+        // fav_count.innerText = `${spots.length} favorited spots`
 
         // expand button for user detail
-        const div = document.createElement('div')
-        const expandBtn = document.createElement('button')
-        expandBtn.innerText = 'Expand'
-        expandBtn.addEventListener('click', () => {
-            this.expandUser(spots, reviews, div)
-        })
+        const btn = document.createElement('div')
+        btn.className = 'ui.bottom.attached.button'
+        const icon = document.createElement('i')
+        icon.className = 'add.icon'
+        // const expandBtn = document.createElement('button')
+        icon.innerText = 'Expand'
+        // expandBtn.addEventListener('click', () => {
+        //     this.expandUser(spots, reviews, div)
+        // })
 
         // append elemnts to card and return the finished element
-        this.element.append(name, username, review_count, fav_count, expandBtn, div)
+        btn.appendChild(icon)
+        content.append(name, details)
+        this.element.append(content, btn)
         
         return this.element
     }
