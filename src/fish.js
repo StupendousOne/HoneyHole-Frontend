d8c3a68570df2b320f00c0b2f116474e39400ce1
StupendousOne <harrisongeo87@gmail.com>
2020-05-28 00:17:38 -0400

    styling for fish done

--- src/fish.js
+++ src/fish.js
@@ -7,7 +7,7 @@ class Fish {
         this.is_active = is_active
         this.image = image
         this.fishingSpots = fishingSpots
-        this.element = document.createElement('card')
+        this.element = document.createElement('div')
     }
 
 
@@ -15,28 +15,87 @@ class Fish {
         // create card and put user title, username, and review content on it
         if(this.is_active){
             // create card and put user name/username on it
-            this.element.className = 'card'
-            const speciesP = document.createElement('p')
-            speciesP.innerText = this.species
-            speciesP.dataset.id = this.id
+            this.element.classList.add('card')
+            this.element.classList.add("h-100")
+
             const fishImg = document.createElement('img')
             fishImg.src = this.image
-            const descP = document.createElement('ul')
+            fishImg.className = "card-img-top"
+
+            const cardBody = document.createElement("div")
+            cardBody.className = "card-body"
+
+            const speciesTitle = document.createElement('h5')
+            speciesTitle.innerText = this.species
+            speciesTitle.dataset.id = this.id
+            speciesTitle.className = "card-title"
+            
+            // <button data-toggle="collapse" data-target="#demo">Collapsible</button>
+            // <div id="demo" class="collapse">
+            // Lorem ipsum dolor text....
+            // </div>
+            const btnDiv = document.createElement("div")
+            const descBtn = document.createElement("button")
+            descBtn.classList.add('btn')
+            descBtn.classList.add('btn-primary')
+            descBtn.dataset.toggle="modal"
+            descBtn.dataset.target=`#infoModal`
+            descBtn.innerText = "Description"
+            descBtn.addEventListener("click",()=>this.fillOutModal())
+            btnDiv.appendChild(descBtn)
+            
+            const descDiv = document.createElement('div')
+            descDiv.className = "collapse"
+            descDiv.id = `fish${this.id}`
+
+            const descP = document.createElement("p")
             descP.innerText = this.description
+
             const spotsUl = document.createElement('ul')
             spotsUl.innerText = "Locations:"
             
-            this.element.append(speciesP, fishImg, descP, spotsUl)
+            descDiv.append(descP, spotsUl)
+            cardBody.append(speciesTitle, btnDiv, descDiv)
+            this.element.append(fishImg, cardBody)
             this.fishingSpots.forEach(fishingSpot => this.listFishingSpot(fishingSpot, spotsUl))
-                
+
             return this.element
         }
     }
+
+    fillOutModal(){
+        const body = document.querySelector("#infoModalBody")
+        const header = document.querySelector("#infoModalTitle")
+        const submit = document.querySelector("#infoModalSubmit")
+
+        //hide submit button
+        submit.setAttribute("style", "display:none")
+
+        //clear any old info
+        while(body.lastChild){
+            body.removeChild(body.lastChild)
+        }
+
+        header.innerHTML = this.species
+
+        const fishImg = document.createElement('img')
+        fishImg.src = this.image
+        fishImg.className = "img-fluid"
+
+        const fishDesc = document.createElement('p')
+        fishDesc.innerText = this.description
+
+        const spotsUl = document.createElement('p')
+        spotsUl.innerText = "Locations:"
+        
+        this.fishingSpots.forEach(fishingSpot => this.listFishingSpot(fishingSpot, spotsUl))
+        body.append(fishImg, fishDesc, spotsUl)
+    }
     
-    listFishingSpot(fishingSpot, ul){
+    listFishingSpot(fishingSpot, p){
         const spotLi = document.createElement('li')
         spotLi.innerText = fishingSpot.name
-        ul.appendChild(spotLi)
+        p.appendChild(spotLi)
     }
 
 }
