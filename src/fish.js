ec976132f407d7553c5e85d6f8fb5c29c70358e2
StupendousOne <harrisongeo87@gmail.com>
2020-05-29 14:54:22 -0400

    review

--- src/fish.js
+++ src/fish.js
@@ -159,7 +159,7 @@ class Fish {
         // Description
         const descLabel = document.createElement("label")
         descLabel.htmlFor = "descField"
-        descLabel.innerText = "Latitude: "
+        descLabel.innerText = "Description: "
 
         const descField = document.createElement("input")
         descField.type = "text"
